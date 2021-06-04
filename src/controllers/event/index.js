45723ea60c0febf5152c1a48d64f4164ee48834d
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2021-06-04 20:44:21 +0700

    add create event and get event info

--- src/controllers/event/index.js
+++ src/controllers/event/index.js
@@ -1,6 +1,18 @@
 let eventController = {};
 
 eventController.createEvent = (req, res) => {
+
+    const { time, name } = req.body;
+
+    if(!name) {
+        res.status(400).send('name is empty');
+        return;
+    }
+
+    res.status(200).send({time, name});
+}
+
+eventController.createEventTicket = (req, res) => {
         const { name, price } = req.body;
 
         if(!price) {
@@ -10,6 +22,11 @@ eventController.createEvent = (req, res) => {
 
         res.status(200).send({name, price});
 
+}
+
+eventController.getEventInfo = (req, res) => {
+
+    res.status(200).send('Success sending request');
 }
 
 module.exports = eventController;
