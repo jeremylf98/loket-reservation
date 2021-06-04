cc27dc81ecbc56c7eecaf28268ecce7c3a3d712f
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2021-06-04 20:06:52 +0700

    add purchase and get_info

---
+++ src/routes/v1/transaction.js
@@ -0,0 +1,24 @@
+const express = require('express');
+let router = express.Router();
+
+router.route('/purchase')
+    .post((req, res) => {
+
+        const { time, name, price } = req.body;
+
+        if(!name) {
+            res.status(400).send('name is empty');
+            return;
+        }
+
+        res.status(200).send({time, name, price});
+    });
+
+router.route('/get_info')
+    .get((req, res) => {
+
+        res.status(200).send('Success sending request');
+        
+    });
+
+module.exports = router;
