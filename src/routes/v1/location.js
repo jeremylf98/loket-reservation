743a8cc66773ac19a97bd033a343f2b4f8ef0a59
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2021-06-04 20:06:34 +0700

    add  create location

---
+++ src/routes/v1/location.js
@@ -0,0 +1,17 @@
+const express = require('express');
+let router = express.Router();
+
+router.route('/create')
+    .post((req, res) => {
+
+        const { name } = req.body;
+
+        if(!name) {
+            res.status(400).send('name is empty');
+            return;
+        }
+
+        res.status(200).send({name});
+    });
+
+module.exports = router;
