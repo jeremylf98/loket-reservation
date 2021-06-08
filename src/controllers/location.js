98a0303bf2f5c33b95ba15480f0a86ba7208115f
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2021-06-08 15:14:37 +0700

    add error message and validation handling

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -1,14 +1,20 @@
 const locationService = require('../services/location');
 const responseHelper = require('../helper/response.helper');
+const locationSchema = require('./Schema/location.schema');
 
 class LocationController{
     async createLocation(req, res) {
         try {
+
+            // validate the body
+            await locationSchema.createEventSchema(req.body);
+
             const data = await locationService.createLocation(req.body);
             return res.status(200).json(responseHelper.baseResponse(data));
 
         } catch (e) {
             console.log(e);
+            res.json(responseHelper.errorResponse(e.message));
         }
     }
 }
