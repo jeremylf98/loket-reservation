5decc1263655ca45191979cb1d42936380c28102
Jeremy Levian Febry <jeremylevian@yahoo.co.id>
2021-06-14 13:01:16 +0700

    remove unnecessary code and add component

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,13 @@ import EventCreate from './fetches/event.create';
 import LocationGetInfo from './fetches/location.get-info';
 import LocationCreate from './fetches/location.create';
 import TransactionGetInfo from './fetches/transaction.get-info';
+import TransactionCreate from './fetches/transaction.create';
 import EventTicketGetInfo from './fetches/event-ticket.get-info';
 import EventTicketCreate from './fetches/event-ticket.create';
 
 import './App.css';
 
 class App extends Component {
-  // constructor(props) {
-  //     super(props);
-  //     this.state = { apiResponse: "" };
-  // }
-
-  // callAPI() {
-  //     fetch("http://localhost:8000/event/get_info/d77e7f91-a252-4b7b-a683-1390e77ce040")
-  //         .then(res => res.text())
-  //         .then(res => this.setState({ apiResponse: res }));
-  // }
-
-  // componentWillMount() {
-  //     this.callAPI();
-  // }
 
   render() {
     return <div className="App">
@@ -33,6 +20,7 @@ class App extends Component {
       <EventGetInfo />
       <EventTicketCreate />
       <EventTicketGetInfo />
+      <TransactionCreate />
       <TransactionGetInfo />
     </div>
   }
